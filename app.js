79fbea638e473f86197458e82c41c243d0759cbe
Ilya Dobatovkin <ramon_mattlov@vulong-development.ru>
2024-04-18 01:04:08 +0300

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+
+  res.status(200)
+  res.json({ "ok": "200" })
+});
+
+app.listen(port, () => console.log(
+  `Express запущен в режиме нажмите ${port} Ctrl + C для завершения.`
+));
